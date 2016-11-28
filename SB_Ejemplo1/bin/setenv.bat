67d45b0367dfb1725c82eb5d44843ee057ea2f85
Nico-pcCasa <nicolas.ard@gmail.com>
2016-11-28 16:55:21 -0300

    Se corrije el setenv.bat y se comienza a armar el ambiente con maven

--- SB_Ejemplo1/bin/setenv.bat
+++ SB_Ejemplo1/bin/setenv.bat
@@ -1,5 +1,18 @@
 @ECHO OFF
-echo SETEANDO VARIABLES DE ENTORNO
+echo SETEANDO VARIABLES DE ENTORNO JAVA Y TOMCAT
 
-SET JAVA_HOME = Progra~2\Java\jdk1.6.0_23
-echo JAVA_HOME: %JAVA_HOME%
+:: SET APP_HOME=C:\Users\Nico\workspace\WORK\GireAPI
+
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_31
+SET JRE_HOME=C:\Program Files\Java\jdk1.8.0_31
+
+:: SET ANT_HOME=C:\Users\Nico\Desktop\ORDENAR\eclipse\apache-ant-1.9.6
+SET MAVEN_HOME=D:\BASE\apache-maven-3.3.9
+
+:: Cosas de catalina tomcat
+:: SET CATALINA_HOME=C:\Users\Nico\Desktop\ORDENAR\eclipse\apache-tomcat-7.0.64
+:: SET CATALINA_BASE=C:\Users\Nico\workspace\WORK\GireAPI
+:: SET CATALINA_TMPDIR=C:\Users\Nico\workspace\WORK\GireAPI
+
+:: Set Path variable
+SET PATH=%Path%;%MAVEN_HOME%\bin;%ANT_HOME%\bin;%JAVA_HOME%\bin;%CATALINA_HOME%;%CATALINA_BASE%
